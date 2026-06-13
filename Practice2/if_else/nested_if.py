3caf700d6f23cd0f0c0cd689c900576347d5450a
Karakat <karakatimangali0@gmail.com>
2026-06-13 10:53:37 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/if_else/nested_if.py
@@ -0,0 +1,11 @@
+#Checking multiple conditions with nesting:
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
---
+++ Practice2/if_else/short_hand_if.py
@@ -0,0 +1,14 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#One-line if/else that prints a value:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+#Finding the maximum of two numbers:
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
