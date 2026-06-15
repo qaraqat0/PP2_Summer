20c297062c3ad6d8410b91130a1980f4f7cbd5ea
Karakat <karakatimangali0@gmail.com>
2026-06-15 22:00:10 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Practice3/functions/args_kwargs.py
@@ -0,0 +1,17 @@
+#Accessing individual arguments from *args:
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
+
+#Accessing values from **kwargs:
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
